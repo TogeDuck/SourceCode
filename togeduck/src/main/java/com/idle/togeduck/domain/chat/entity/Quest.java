811bbbac84e6f9610fe3c3117d50ab20fdc55331
차재환 <barded1998@gmail.com>
2024-02-01 22:40:00 +0900

    feat-backend: Quest, Chat 분리

--- togeduck/src/main/java/com/idle/togeduck/domain/chat/entity/Quest.java
+++ togeduck/src/main/java/com/idle/togeduck/domain/chat/entity/Quest.java
@@ -1,44 +1,42 @@
 package com.idle.togeduck.domain.chat.entity;
 
-import java.time.LocalDateTime;
-
 import com.idle.togeduck.domain.BaseEntity;
+import com.idle.togeduck.domain.event.entity.Event;
+import com.idle.togeduck.domain.user.entity.User;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorColumn;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 @Entity
 @Getter
-@Builder
+@SuperBuilder
 @NoArgsConstructor
-@AllArgsConstructor
-public class Chat extends BaseEntity {
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn
+public class Quest extends BaseEntity {
 
 	@Id
 	@GeneratedValue
-	@Column(name = "chat_id")
+	@Column(name = "quest_id")
 	private Long id;
 
 	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "quest_id")
-	private Quest quest;
+	@JoinColumn(name = "event_id")
+	private Event event;
 
 	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "trade_id")
-	private Quest trade;
+	@JoinColumn(name = "user_id")
+	private User user;
 
-	private Integer maximum;
-
-	private Integer duration;
-
-	private LocalDateTime expiredAt;
 }
--- togeduck/src/main/java/com/idle/togeduck/domain/chat/entity/Talk.java
+++ togeduck/src/main/java/com/idle/togeduck/domain/chat/entity/Talk.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Talk extends Chat {
+public class Talk extends Quest {
 
 	private String name;
 }
