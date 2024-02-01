1d7ed96cae14949d583290611b8158905b58ff15
AhyngK <130175561+AhyngK@users.noreply.github.com>
2024-02-01 12:55:25 +0900

    feat-android: quest-talk Fragment 개발 진행 중 S10P12A301-233

--- Android/TogeDuck/app/src/main/java/com/idle/togeduck/view/quest_talk/QuestTalkViewHolder.kt
+++ Android/TogeDuck/app/src/main/java/com/idle/togeduck/view/quest_talk/QuestTalkViewHolder.kt
@@ -36,15 +36,15 @@ class QuestTalkViewHolder (
     }
 
     private fun setTheme(questTalk: QuestTalk, context: Context) {
-        val squareCircle = ContextCompat.getDrawable(context, R.drawable.shape_square_circle) as GradientDrawable
+        val roundSquare = ContextCompat.getDrawable(context, R.drawable.shape_all_round_20) as GradientDrawable
         val circle = ContextCompat.getDrawable(context, R.drawable.shape_circle) as GradientDrawable
-        squareCircle.setColor(ContextCompat.getColor(context, Theme.theme.sub200))
-        squareCircle.setStroke(0,0)
+        roundSquare.setColor(ContextCompat.getColor(context, Theme.theme.sub200))
+        roundSquare.setStroke(0,0)
         circle.setColor(ContextCompat.getColor(context, Theme.theme.sub500))
         circle.setStroke(0,0)
-        questTalkLayout.background = squareCircle
         questTalkIconContainer.background = circle
         questTalkUser.setText(questTalk.user)
+        questTalkContent.background = roundSquare
         questTalkContent.setText(questTalk.content)
     }
 }
