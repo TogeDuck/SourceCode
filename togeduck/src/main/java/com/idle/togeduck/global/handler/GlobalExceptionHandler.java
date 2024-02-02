02a4ba3f24916d6c11c97fca778a02bcb0a64a21
차재환 <barded1998@gmail.com>
2024-02-02 11:26:48 +0900

    fix-backend: controller advice 수정

--- togeduck/src/main/java/com/idle/togeduck/global/handler/GlobalExceptionHandler.java
+++ togeduck/src/main/java/com/idle/togeduck/global/handler/GlobalExceptionHandler.java
@@ -1,15 +1,9 @@
 package com.idle.togeduck.global.handler;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.annotation.SendToUser;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -24,8 +18,6 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class GlobalExceptionHandler {
 
-	private final SimpMessagingTemplate template;
-
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<ErrorResponse> handleException(Exception e) {
 		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
@@ -35,17 +27,6 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(BaseException.class)
 	public ResponseEntity<ErrorResponse> handleBaseException(BaseException e) {
 		return ResponseEntity.status(e.getStatus()).body(new ErrorResponse(e.getStatus(), e.getCode()));
-	}
-
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<Map<String, String>> dtoValidation(final MethodArgumentNotValidException e) {
-		Map<String, String> errors = new HashMap<>();
-		e.getBindingResult().getAllErrors().forEach((error) -> {
-			String fieldName = ((FieldError)error).getField();
-			String errorMessage = error.getDefaultMessage();
-			errors.put(fieldName, errorMessage);
-		});
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
 	}
 
 	@MessageExceptionHandler(BaseException.class)
