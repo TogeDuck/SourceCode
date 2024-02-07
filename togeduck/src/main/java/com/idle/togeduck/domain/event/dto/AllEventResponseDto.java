43d80dd5f0cbae2d07fbf699cace7f94fd1fc27d
차재환 <barded1998@gmail.com>
2024-02-07 14:58:08 +0900

    Update AllEventResponseDto.java

--- togeduck/src/main/java/com/idle/togeduck/domain/event/dto/AllEventResponseDto.java
+++ togeduck/src/main/java/com/idle/togeduck/domain/event/dto/AllEventResponseDto.java
@@ -3,8 +3,8 @@ package com.idle.togeduck.domain.event.dto;
 import java.util.List;
 
 public record AllEventResponseDto(
-	List<?> past,
-	List<?> today,
-	List<?> later
+	List<JoinEventResponseDto> past,
+	List<JoinEventResponseDto> today,
+	List<JoinEventResponseDto> later
 ) {
 }
