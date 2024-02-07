00dd889486def077556ac72bd2efb4f4152e5896
Ahyoung Kim <130175561+AhyngK@users.noreply.github.com>
2024-02-08 01:45:05 +0900

    feat-android: 소켓 연결 개발 진행

--- Android/TogeDuck/app/src/main/java/com/idle/togeduck/MainFragment.kt
+++ Android/TogeDuck/app/src/main/java/com/idle/togeduck/MainFragment.kt
@@ -1,5 +1,6 @@
 package com.idle.togeduck
 
+import android.content.ContentValues.TAG
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
@@ -10,6 +11,7 @@ import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.work.ExistingPeriodicWorkPolicy
 import androidx.work.PeriodicWorkRequestBuilder
@@ -23,6 +25,8 @@ import com.idle.togeduck.main_map.MapViewModel
 import com.idle.togeduck.network.Coordinate
 import com.idle.togeduck.network.Message
 import com.idle.togeduck.network.Quest
+import com.idle.togeduck.network.StompManager
+import com.idle.togeduck.network.StompManagerTest
 import com.idle.togeduck.network.WebSocketManager
 import com.idle.togeduck.quest.share.ShareViewModel
 import com.idle.togeduck.util.GPSWorker
@@ -33,6 +37,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import ua.naiksoftware.stomp.dto.StompHeader
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoUnit
@@ -51,9 +56,6 @@ class MainFragment : Fragment() {
     private val shareViewModel: ShareViewModel by activityViewModels()
     private val mapViewModel: MapViewModel by activityViewModels()
     private val favoriteSettingViewModel: FavoriteSettingViewModel by activityViewModels()
-
-    private var temp1 = false
-    private var temp2 = false
 
     lateinit var webSocketManager : WebSocketManager
     lateinit var webSocketManager1 : WebSocketManager
@@ -83,43 +85,42 @@ class MainFragment : Fragment() {
             findNavController().navigate(R.id.action_mainFragment_to_mapFragment)
         }
 
+        val stompManager = StompManager()
+        val stompManagerTest = StompManagerTest()
+
         binding.btn1.setOnClickListener {
-            if (!temp1) {
-                webSocketManager.connect()
-                webSocketManager.subscribe("/sub/chats/1") { message ->
-                    requireActivity().runOnUiThread {
-                        questToast(message)
-                    }
-                }
-                temp1 = true
-            } else {
-                temp1 = false
-                webSocketManager.disconnect()
+            Log.d("버튼","김아영 버튼 눌림")
+
+            // Stomp 연결
+            val headers = listOf(
+                com.idle.togeduck.websocketcustomlibrary.dto.StompHeader("Authorization", "guest")
+            )
+//            stompManager.connect(headers)
+            stompManagerTest.connect(headers)
+
+            // 특정 토픽에 대한 구독
+            stompManagerTest.subscribeTopic("/sub/chats/1") { message ->
+                questToast(message)
+                Log.d("웹소켓 1", "Received message: $message")
             }
         }
 
         binding.btn2.setOnClickListener {
-//            if (!temp2) {
-//                webSocketManager1.connect()
-//                webSocketManager1.subscribe("/topic/coors") { message ->
-//                    coor(message)
-//                }
-//                temp2 = true
-//                Log.d("웹소켓 연결", "연결됨")
-//            } else {
-//                temp2 = false
-//                webSocketManager1.disconnect()
-//                Log.d("웹소켓 좌표 종료", "연결끊김")
-//            }
-            webSocketManager1.connect()
-            webSocketManager1.send("/pub/chat/1/message", 1L, "안녕하세요")
+            Log.d("버튼","이지우 버튼 눌림")
+            if(webSocketManager1.getConnectedState()){
+                Log.d("버튼","웹소켓 1번 연결됨")
+                webSocketManager1.send("/pub/chats/1/message", 1, "안녕하세요")
+            }
         }
 
         binding.btn3.setOnClickListener {
-//            findNavController().navigate(R.id.action_mainFragment_to_FavoriteSettingFragment)
-            CoroutineScope(Dispatchers.IO).launch {
-                shareViewModel.getShareList(1, 0, 5)
-            }
+            Log.d("버튼","최지찬 버튼 눌림")
+            val destination = "/pub/chats/1/message"
+            val payload = "Hello, WebSocket!"
+            val headers = listOf(
+                com.idle.togeduck.websocketcustomlibrary.dto.StompHeader("Authorization", "guest")
+            )
+            stompManagerTest.send(destination,1, payload, headers)
         }
         //----------------------------------------------------
     }
--- Android/TogeDuck/app/src/main/java/com/idle/togeduck/network/WebSocketManager.kt
+++ Android/TogeDuck/app/src/main/java/com/idle/togeduck/network/WebSocketManager.kt
@@ -4,50 +4,56 @@ import android.util.Log
 import com.google.gson.Gson
 import com.idle.togeduck.di.PreferenceModule
 import io.reactivex.disposables.CompositeDisposable
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import ua.naiksoftware.stomp.Stomp
+import ua.naiksoftware.stomp.StompClient
 import ua.naiksoftware.stomp.dto.LifecycleEvent
 import ua.naiksoftware.stomp.dto.StompHeader
 import javax.inject.Inject
 
 class WebSocketManager @Inject constructor(private val preference: PreferenceModule) {
-    val url = "wss://i10a301.p.ssafy.io/ws-stomp"
-    val stompClient = Stomp.over(Stomp.ConnectionProvider.OKHTTP, url)
+//    val url = "wss://i10a301.p.ssafy.io/ws-stomp"
+    val url = "ws://10.0.2.2:8080/ws-stomp"
+    lateinit var stompClient : StompClient
     var accessToken: String? = null
-    val headerList = arrayListOf<StompHeader>()
+    lateinit var headerMap: Map<String, String>
+    private var isConnected = false
 
     init {
-        accessToken = runBlocking {
-            preference.getAccessToken.first()
+        headerMap = mapOf()
+        GlobalScope.launch(Dispatchers.Main) {
+            accessToken = withContext(Dispatchers.IO) {
+                preference.getAccessToken.first() // 비동기로 AccessToken을 가져오기 위해 Dispatchers.IO를 사용합니다.
+            }
+            accessToken?.let {
+                headerMap = mapOf(
+                    Pair("Authorization", it)
+                )
+            }
+
+            // Stomp 클라이언트 초기화 및 연결
+            stompClient = Stomp.over(Stomp.ConnectionProvider.OKHTTP, url, headerMap)
         }
-        headerList.add(StompHeader("Authorization",accessToken))
     }
 
-    fun connect(){
-        Log.d("웹소켓",accessToken.toString())
-        Log.d("웹소켓",headerList.toString())
-        stompClient.connect(headerList)
-
-        stompClient.lifecycle().subscribe { lifecycleEvent ->
-            when (lifecycleEvent.type) {
-                LifecycleEvent.Type.OPENED -> {
-                    Log.d("웹소켓", "WebSocket Opened")
-                }
-                LifecycleEvent.Type.CLOSED -> {
-                    Log.d("웹소켓", "WebSocket Closed")
-
-                }
-                LifecycleEvent.Type.ERROR -> {
-                    Log.d("웹소켓", "WebSocket Error")
-                    Log.d("웹소켓", "네트워크 오류 ${lifecycleEvent.exception.toString()}")
-                    disconnect()
-                }
-                else ->{
-                    Log.d("웹소켓", lifecycleEvent.message)
-                }
-            }
+    suspend fun connect(): Boolean {
+        var result = false
+        lifeCycleLog()
+        try {
+            stompClient.connect()
+            isConnected = true
+            result = true
+        } catch (e: Exception) {
+            isConnected = false
+            result = false
         }
+        return result
     }
 
     fun subscribe(url: String, callback: (String) -> Unit) {
@@ -64,8 +70,10 @@ class WebSocketManager @Inject constructor(private val preference: PreferenceMod
 
     fun send(destination:String, chatId: Long, message: String){
         var messageRequest = Message(chatId, message)
+        Log.d("웹소켓 메세지 전송 시도", message)
         try {
-            stompClient.send(destination, Gson().toJson(messageRequest))
+            Log.d("메세지 목적지", destination)
+            stompClient.send(destination, Gson().toJson(messageRequest)).subscribe()
         }
         catch (e: Exception){
             Log.d("웹소켓", "메세지 전송 실패")
@@ -73,6 +81,32 @@ class WebSocketManager @Inject constructor(private val preference: PreferenceMod
     }
 
     fun disconnect(){
+        isConnected = false
         stompClient.disconnect()
+    }
+
+    fun getConnectedState(): Boolean{
+        return isConnected
+    }
+
+    fun lifeCycleLog(){
+        stompClient.lifecycle().subscribe { lifecycleEvent ->
+            when (lifecycleEvent.type) {
+                LifecycleEvent.Type.OPENED -> {
+                    Log.d("웹소켓", "WebSocket Opened")
+                }
+                LifecycleEvent.Type.CLOSED -> {
+                    Log.d("웹소켓", "WebSocket Closed")
+                }
+                LifecycleEvent.Type.ERROR -> {
+                    Log.d("웹소켓", "WebSocket Error")
+                    Log.d("웹소켓", "네트워크 오류 ${lifecycleEvent.exception.toString()}")
+                    disconnect()
+                }
+                else ->{
+                    Log.d("웹소켓", lifecycleEvent.message)
+                }
+            }
+        }
     }
 }
