bdba5162b70cfddc7b294a6fa831c3e21cf42f3e
나호균 <nahk99@naver.com>
2024-02-07 14:23:02 +0900

    feat-backend: 유저 정보 등록, history, chat, event 수정

--- togeduck/src/main/java/com/idle/togeduck/domain/chat/controller/ChatController.java
+++ togeduck/src/main/java/com/idle/togeduck/domain/chat/controller/ChatController.java
@@ -8,12 +8,14 @@ import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.idle.togeduck.domain.chat.dto.ChatRoomResponseDto;
 import com.idle.togeduck.domain.chat.dto.MessageDto;
 import com.idle.togeduck.domain.chat.service.ChatService;
+import com.idle.togeduck.domain.user.entity.User;
 import com.idle.togeduck.global.response.BaseResponse;
 
 import lombok.RequiredArgsConstructor;
@@ -27,10 +29,10 @@ public class ChatController {
 	private final ChatService chatService;
 
 	@GetMapping("/events/chats")
-	public ResponseEntity<BaseResponse<List<ChatRoomResponseDto>>> getChatRooms(Long userId) {
+	public ResponseEntity<BaseResponse<List<ChatRoomResponseDto>>> getChatRooms(@AuthenticationPrincipal User user) {
 
 		return ResponseEntity.status(HttpStatus.OK)
-			.body(new BaseResponse<>(200, "채팅방 정보를 가져왔습니다", chatService.getChatRooms(userId)));
+			.body(new BaseResponse<>(200, "채팅방 정보를 가져왔습니다", chatService.getChatRooms(user.getId())));
 	}
 
 	//모집 참여
