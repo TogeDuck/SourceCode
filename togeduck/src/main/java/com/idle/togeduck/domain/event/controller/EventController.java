b1b1e5b55ffc155e9f0fb3ca2a9bd647c263efc6
나호균 <nahk99@naver.com>
2024-02-07 13:09:19 +0900

    feat-backend: 유저 정보 등록

--- togeduck/src/main/java/com/idle/togeduck/domain/event/controller/EventController.java
+++ togeduck/src/main/java/com/idle/togeduck/domain/event/controller/EventController.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.idle.togeduck.domain.event.dto.TodayAndLaterEventResponseDto;
 import com.idle.togeduck.domain.event.service.EventService;
+import com.idle.togeduck.domain.user.entity.User;
 import com.idle.togeduck.global.response.BaseResponse;
 
 import lombok.RequiredArgsConstructor;
@@ -26,11 +28,12 @@ public class EventController {
 		@RequestParam(name = "celebrity-id") Long celebrityId,
 		@RequestParam(name = "start-date") LocalDate startDate,
 		@RequestParam(name = "end-date") LocalDate endDate,
-		Long userId) {
+		@AuthenticationPrincipal User user) {
 
 		return ResponseEntity
 			.status(HttpStatus.OK)
 			.body(
-				new BaseResponse<>(200, "이벤트 조회 완료", eventService.getEvents(celebrityId, startDate, endDate, userId)));
+				new BaseResponse<>(200, "이벤트 조회 완료",
+					eventService.getEvents(celebrityId, startDate, endDate, user.getId())));
 	}
 }
