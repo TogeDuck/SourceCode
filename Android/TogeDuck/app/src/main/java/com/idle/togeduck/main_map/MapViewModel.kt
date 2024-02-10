7c4301022f1ec94c4869a9789d9bd7e2cdff3511
Ahyoung Kim <130175561+AhyngK@users.noreply.github.com>
2024-02-10 23:47:27 +0900

    feat-android: WebSocket 관리 부분 변경 중

--- Android/TogeDuck/app/src/main/java/com/idle/togeduck/main_map/MapViewModel.kt
+++ Android/TogeDuck/app/src/main/java/com/idle/togeduck/main_map/MapViewModel.kt
@@ -5,15 +5,18 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.idle.togeduck.MessageKind
 import com.idle.togeduck.event.model.Event
 import com.idle.togeduck.favorite.model.Celebrity
 import com.idle.togeduck.history.model.Position
-import com.idle.togeduck.main_map.view.MessageKind
 import com.idle.togeduck.network.Coordinate
 import com.idle.togeduck.util.CalcDistance
+import com.idle.togeduck.util.DpPxUtil
 import com.idle.togeduck.util.NaverItem
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.NaverMap
+import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.overlay.OverlayImage
 import dagger.hilt.android.lifecycle.HiltViewModel
 import java.time.LocalDate
 import javax.inject.Inject
@@ -28,8 +31,8 @@ class MapViewModel @Inject constructor(
     private val _pickedDate = MutableLiveData<Pair<LocalDate, LocalDate>>()
     val pickedDate: LiveData<Pair<LocalDate, LocalDate>> get() = _pickedDate
 
-    private val _peopleMarkerList = MutableLiveData<Map<String, NaverItem>>()
-    val peopleMarkerList : MutableLiveData<Map<String, NaverItem>> get() = _peopleMarkerList
+    private val _peopleMarkerList = MutableLiveData<Map<String, Marker>>()
+    val peopleMarkerList : MutableLiveData<Map<String, Marker>> get() = _peopleMarkerList
 
     private val _isTourStart = MutableLiveData<Boolean>(false)
     val isTourStart get() = _isTourStart
@@ -40,8 +43,19 @@ class MapViewModel @Inject constructor(
     private val _closeEvents = MutableLiveData<List<Event>>()
     val closeEvents: LiveData<List<Event>> get() = _closeEvents
 
+    private var peopleMarkerOverlay: OverlayImage? = null
+    var markerSize: Int = 20
+
+    var peopleNum: MutableLiveData<Int> = MutableLiveData(0)
+
+    var naverMap:NaverMap? = null
+
+
     init{
         _peopleMarkerList.value = emptyMap()
+    }
+    fun initPeopleMarkerImage(image: OverlayImage){
+        this.peopleMarkerOverlay = image
     }
 
     fun initTourList(){
@@ -113,20 +127,55 @@ class MapViewModel @Inject constructor(
         val updateMap = _peopleMarkerList.value?.toMutableMap() ?: mutableMapOf()
         when(type){
             MessageKind.MESSAGE.toString() -> {
-                updateMap[coordinate.userId] = NaverItem(coordinate.latitude, coordinate.longitude)
+                updateMap[coordinate.userId]?.map = null
+                val marker = Marker()
+                marker.position = LatLng(coordinate.latitude, coordinate.longitude)
+                marker.icon = peopleMarkerOverlay!!
+                marker.alpha = 0.5f
+                marker.map = naverMap
+                marker.height = markerSize
+                marker.width = markerSize
+                updateMap[coordinate.userId] = marker
             }
             MessageKind.LEAVE.toString() -> {
+                updateMap[coordinate.userId]!!.map = null
                 updateMap.remove(coordinate.userId)
             }
+        }
+        val currentPeopleMarkerMap = peopleMarkerList.value
+        peopleNum.value = if (currentPeopleMarkerMap != null) {
+            currentPeopleMarkerMap.size
+        } else {
+            0
         }
         _peopleMarkerList.postValue(updateMap)
     }
 
-    fun clearList() {
-        _peopleMarkerList.value = mapOf()
+    fun updateMarkerSize() {
+        _peopleMarkerList.value?.let { peopleMarkers ->
+            for ((_, marker) in peopleMarkers) {
+                marker?.let {
+                    it.width = markerSize
+                    it.height = markerSize
+                }
+            }
+        }
     }
 
+    fun deleteAllMarkers() {
+        _peopleMarkerList.value?.let { peopleMarkers ->
+            for ((_, marker) in peopleMarkers) {
+                marker?.let {
+                    it.map = null
+                }
+            }
+        }
+        _peopleMarkerList.postValue(emptyMap())
+    }
     fun setTourStatus(isStart: Boolean) {
         _isTourStart.value = isStart
+    }
+    fun clearList() {
+        _peopleMarkerList.value = mapOf()
     }
 }
