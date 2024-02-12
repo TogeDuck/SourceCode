a3e58ca205da851e12f3234bd1ec526c300a55aa
최지찬 <pnlkc@naver.com>
2024-02-12 14:11:37 +0900

    fix-android:로그인 시 userId 받아오도록 변경

--- Android/TogeDuck/app/src/main/java/com/idle/togeduck/login/model/LoginDataResponse.kt
+++ Android/TogeDuck/app/src/main/java/com/idle/togeduck/login/model/LoginDataResponse.kt
@@ -5,6 +5,7 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class LoginDataResponse(
     val grantType: String,
+    val userId: Long,
     val accessToken: String,
     val refreshToken: String,
     val accessTokenExpireDate: Long
