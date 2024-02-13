72743d5fb31e8ea465e5bedc4a1f0fd6c95cdd82
나호균 <nahk99@naver.com>
2024-02-13 12:42:36 +0900

    fix-backend: 관심연예인 등록,삭제 로직 변경

--- togeduck/src/main/java/com/idle/togeduck/domain/user/controller/UserController.java
+++ togeduck/src/main/java/com/idle/togeduck/domain/user/controller/UserController.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,15 +36,14 @@ public class UserController {
 			));
 	}
 
-	@PostMapping("/favorites")
-	public ResponseEntity<BaseResponse<?>> upsertFavorite(
+	@PatchMapping("/favorites")
+	public ResponseEntity<BaseResponse<Void>> upsertFavorite(
 		@RequestBody FavoriteRequestDto favoriteRequestDto, @AuthenticationPrincipal User user) { // update + insert
 
 		userService.upsertFavorite(favoriteRequestDto, user.getId());
-
 		return ResponseEntity
 			.status(HttpStatus.OK)
-			.body(new BaseResponse<>(200, "success", getFavorite(user).getBody().getData()));
+			.body(new BaseResponse<>(200, "관심연예인 변경이 성공했습니다", null));
 	}
 
 }
