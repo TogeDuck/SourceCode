a571be21c55401d3cf36c81fe0fa21f1cb86251d
나호균 <nahk99@naver.com>
2024-02-13 15:32:52 +0900

    fix-backend: 두군데 이상 방문한 경로만 출력해주도록 수정

--- togeduck/src/main/java/com/idle/togeduck/domain/user/repository/HistoryRepository.java
+++ togeduck/src/main/java/com/idle/togeduck/domain/user/repository/HistoryRepository.java
@@ -13,7 +13,7 @@ public interface HistoryRepository extends JpaRepository<History, Long> {
 	@Query("select new com.idle.togeduck.domain.user.dto.HistoryResponseDto("
 		+ "h.id, h.name, h.date) "
 		+ "from History h "
-		+ "where h.celebrityId = :celebrityId and h.user.id = :userId "
+		+ "where h.celebrityId = :celebrityId and h.user.id = :userId and length(h.route)> 90 "
 		+ "order by h.createdAt desc")
 	List<HistoryResponseDto> findMyHistory(Long celebrityId, Long userId);
 }
