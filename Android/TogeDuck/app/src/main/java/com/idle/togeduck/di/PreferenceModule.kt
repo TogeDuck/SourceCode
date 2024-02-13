3ce762e28db9891e152d9c173e21ab3f84f403e4
최지찬 <pnlkc@naver.com>
2024-02-13 10:24:30 +0900

    feat-android:채팅방 정보 로컬 저장

--- Android/TogeDuck/app/src/main/java/com/idle/togeduck/di/PreferenceModule.kt
+++ Android/TogeDuck/app/src/main/java/com/idle/togeduck/di/PreferenceModule.kt
@@ -5,13 +5,19 @@ import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.emptyPreferences
 import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.lifecycle.MutableLiveData
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
+import com.idle.togeduck.di.PreferenceModule.PreferenceKeys.KEY_CHAT_ROOM_LIST
+import com.idle.togeduck.di.PreferenceModule.PreferenceKeys.KEY_CHAT_ROOM_TALK_LIST
 import com.idle.togeduck.di.PreferenceModule.PreferenceKeys.KEY_GUID
 import com.idle.togeduck.di.PreferenceModule.PreferenceKeys.KEY_JWT_ACCESS_TOKEN_TOKEN
 import com.idle.togeduck.di.PreferenceModule.PreferenceKeys.KEY_JWT_REFRESH_TOKEN
 import com.idle.togeduck.di.PreferenceModule.PreferenceKeys.KEY_SELECTED_CELEBRITY
 import com.idle.togeduck.favorite.model.Celebrity
+import com.idle.togeduck.quest.talk.model.Talk
+import com.idle.togeduck.quest.talk.model.TalkRoom
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
 import kotlinx.serialization.json.Json
@@ -24,6 +30,9 @@ const val JWT_REFRESH_TOKEN_NAME = "JWT_REFRESH_TOKEN"
 const val JWT_ACCESS_TOKEN_NAME = "JWT_ACCESS_TOKEN"
 const val GUID_NAME = "GUID"
 const val SELECTED_CELEBRITY_NAME = "SELECTED_CELEBRITY"
+const val CHAT_ROOM_LIST_NAME = "CHAT_ROOM_LIST"
+const val CHAT_ROOM_TALK_LIST_NAME = "CHAT_ROOM_TALK_LIST"
+
 
 @Singleton
 class PreferenceModule @Inject constructor(
@@ -117,10 +126,62 @@ class PreferenceModule @Inject constructor(
         }
     }
 
+    val getChatRoomList = dataStore.data
+        .catch { exception ->
+            if (exception is IOException) {
+                emit(emptyPreferences())
+            }
+        }.map { preferences ->
+            val gson = GsonBuilder().create()
+            gson.fromJson(preferences[KEY_CHAT_ROOM_LIST], mutableMapOf<Long, TalkRoom>().javaClass)
+        }
+
+    suspend fun setChatRoomList(chatRoomList: MutableMap<Long, TalkRoom>) {
+        val gson = GsonBuilder().create()
+        val json = gson.toJson(chatRoomList)
+
+        dataStore.edit { preferences ->
+            preferences[KEY_CHAT_ROOM_LIST] = json
+        }
+    }
+
+    suspend fun removeChatRoomList() {
+        dataStore.edit { preferences ->
+            preferences.remove(KEY_CHAT_ROOM_LIST)
+        }
+    }
+
+    val getChatRoomTalkList = dataStore.data
+        .catch { exception ->
+            if (exception is IOException) {
+                emit(emptyPreferences())
+            }
+        }.map { preferences ->
+            val gson = GsonBuilder().create()
+            gson.fromJson(preferences[KEY_CHAT_ROOM_TALK_LIST], mutableMapOf<Long, MutableList<Talk>>().javaClass)
+        }
+
+    suspend fun setChatRoomTalkList(chatRoomList: MutableMap<Long, MutableList<Talk>>) {
+        val gson = GsonBuilder().create()
+        val json = gson.toJson(chatRoomList)
+
+        dataStore.edit { preferences ->
+            preferences[KEY_CHAT_ROOM_TALK_LIST] = json
+        }
+    }
+
+    suspend fun removeChatRoomTalkList() {
+        dataStore.edit { preferences ->
+            preferences.remove(KEY_CHAT_ROOM_TALK_LIST)
+        }
+    }
+
     private object PreferenceKeys {
         val KEY_JWT_REFRESH_TOKEN = stringPreferencesKey(JWT_REFRESH_TOKEN_NAME)
         val KEY_JWT_ACCESS_TOKEN_TOKEN = stringPreferencesKey(JWT_ACCESS_TOKEN_NAME)
         val KEY_GUID = stringPreferencesKey(GUID_NAME)
         val KEY_SELECTED_CELEBRITY = stringPreferencesKey(SELECTED_CELEBRITY_NAME)
+        val KEY_CHAT_ROOM_LIST = stringPreferencesKey(CHAT_ROOM_LIST_NAME)
+        val KEY_CHAT_ROOM_TALK_LIST = stringPreferencesKey(CHAT_ROOM_TALK_LIST_NAME)
     }
 }
