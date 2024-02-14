554ba5b3765691a8ef1d2ddb41d20fdde15b1220
차재환 <barded1998@gmail.com>
2024-02-14 18:04:10 +0900

    feat-back: fcm 수정

--- togeduck/src/main/java/com/idle/togeduck/domain/chat/repository/DealRepository.java
+++ togeduck/src/main/java/com/idle/togeduck/domain/chat/repository/DealRepository.java
@@ -8,6 +8,6 @@ import org.springframework.data.jpa.repository.Query;
 import com.idle.togeduck.domain.chat.entity.Deal;
 
 public interface DealRepository extends JpaRepository<Deal, Long> {
-	@Query("select d from Deal d join fetch d.target t join fetch t.user join fetch d.myTrade m join fetch m.user where d.id = :dealId")
+	@Query("select d from Deal d join fetch d.target t join fetch t.user tu join fetch d.myTrade m join fetch m.user mu where d.id = :dealId")
 	Optional<Deal> findByIdWithTarget(Long dealId);
 }
